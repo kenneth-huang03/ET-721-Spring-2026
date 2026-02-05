ef58635a886f8268754a65bef02465e7c374296f
Kenneth Huang <kenneth.huang03@student.qcc.cuny.edu>
2026-02-05 18:48:10 +0000

    Lab05

---
+++ lab/05/lab05.py
@@ -0,0 +1,24 @@
+"""
+Kenneth Huang
+Feb 5, 2026
+Lab 5 | Functions
+"""
+from lab05_functions import *
+
+print("Example 1: User Defined Functions")
+
+height = 2
+width = 10
+area = area_rectangle(height, width)
+display_result(height, width, area)
+
+print("Example 2: Calculate Distance Between Two Points")
+
+x1 = collect_number("x1")
+y1 = collect_number("y1")
+x2 = collect_number("x2")
+y2 = collect_number("y2")
+
+distance = calculate_distance_between_two_points(x1, y1, x2, y2)
+
+display_distance_result(x1, y1, x2, y2, distance)
---
+++ lab/05/lab05_functions.py
@@ -0,0 +1,23 @@
+import math
+
+# Example 1
+def area_rectangle(height, width):
+    return height * width
+
+def display_result(height, width, area):
+    print(f"In a rectangle with width of {width} units and height of {height} units the area is {area} units.")
+
+# Example 2
+def collect_number(coordinate_name):
+    number = int(input(f"Enter a number between 1-10 for {coordinate_name}:\n> "))
+
+    while (number <= 1 or number >= 10):
+        number = int(input(f"Oops!\nPlease enter a number between 1 and 10 for {coordinate_name}:\n> "))
+
+    return number
+
+def calculate_distance_between_two_points(x1, y1, x2, y2):
+    return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+
+def display_distance_result(x1, y1, x2, y2, distance):
+    print(f"Distance between the points ({x1}, {y1}) and ({x2}, {y2}) is {round(distance, 2)}")
